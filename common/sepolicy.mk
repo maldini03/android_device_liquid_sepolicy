922e478bb07ff910f463a53a52260f06f85d5dc4
liquid0624 <rjbirnesser@gmail.com>
2018-09-24 19:48:03 -0400

    sepolicy: rename liquid sepolicy
    Signed-off-by: liquid0624 <rjbirnesser@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,13 +1,13 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from LiquidRemix
 #
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/public
+    device/liquid/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/private
+    device/liquid/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/vendor
+    device/liquid/sepolicy/common/vendor
